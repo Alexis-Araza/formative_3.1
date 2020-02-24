660faa4f755d97782c0df838ed813971d4f320f3
Alexis-Araza <atmaraza@gmail.com>
2020-02-24 15:27:42 +1300

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,15 +56,74 @@ grunt.initConfig({
 		},
 	},
 
+
+	htmlmin: {                                     // Task
+		dist: {                                      // Target
+			options: {                                 // Target options
+			removeComments: true,
+			collapseWhitespace: true
+			},
+			files: {                                   // Dictionary of files
+			'dist/index.html': 'src/index.html',     // 'destination': 'source'
+			'dist/contact.html': 'src/contact.html'
+			}
+		},
+		dev: {                                       // Another target
+			files: {
+			'dist/index.html': 'src/index.html',
+			'dist/contact.html': 'src/contact.html'
+			}
+		}
+	},
+
+
+	imagemin: {
+		static: {
+			options: {
+			optimizationLevel: 3,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [mozjpeg()] // Example plugin usage
+			},
+			files: {
+			'dist/img.png': 'src/img.png',
+			'dist/img.jpg': 'src/img.jpg',
+			'dist/img.gif': 'src/img.gif'
+			}
+		},
+		dynamic: {
+			files: [{
+			expand: true,
+			cwd: 'src/',
+			src: ['**/*.{png,jpg,gif}'],
+			dest: 'dist/'
+			}]
+		}
+	},
+
+
+	cssmin: {
+		target: {
+			files: [{
+			expand: true,
+			cwd: 'release/css',
+			src: ['*.css', '!*.min.css'],
+			dest: 'release/css',
+			ext: '.min.css'
+			}]
+		}
+	},
+
 });
 
   // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
 
@@ -73,6 +132,9 @@ grunt.initConfig({
   grunt.registerTask('ugly', ['uglify']);
   grunt.registerTask('test', ['watch']);
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('html', ['htmlmin']);
+  grunt.registerTask('image', ['imagemin']);
+  grunt.registerTask('style', ['cssmin']);
   // 'default' checks everything, but can be renamed ie. default = test, [grunt] = [grunt test]
 
 };
